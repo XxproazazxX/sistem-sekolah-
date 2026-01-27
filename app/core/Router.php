442caebcfac6f98513e03307be8e8ad503d2e7d1
XxproazazxX <ethanfrancis2772@gmail.com>
2026-01-27 14:30:08 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -5,6 +5,22 @@ class Router
 {
     public function run()
    {
-      echo "Router is running";
+      $method = $_SERVER['REQUEST_METHOD'];
+      $uri= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+      
+        if ($method =='GET' && $uri == '/Students'){
+            echo'<h1>daftar siswa</h1>';
+            echo'<p>menampilkan daftar siswa</p>';
+        return;
+        }
+         if ($method =='GET' && $uri == '/Students/create') {
+            echo'<h1>Tambah siswa</h1>';
+            echo'<p>menampilkan form tambah siswa</p>';
+        return;
+        }
+
+        http_response_code(404);
+        echo'<h1>404 Not Found</h1>';
+        
     }
 }
